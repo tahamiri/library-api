749c9dfe7d62156ce535866e140860a5348afb7c
taha miri <63375111+tahamiri@users.noreply.github.com>
2023-08-01 13:44:17 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,15 +12,6 @@ from app.db.mongodb_utills import close_mongo_connection, connect_to_mongo
 
 app = FastAPI()
 
-# Set all CORS enabled origins
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.BACKEND_CORS_ORIGINS,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
 
 app.add_event_handler("startup", utills.create_start_app_handler(app))
 app.add_event_handler("shutdown", utills.create_stop_app_handler(app))
